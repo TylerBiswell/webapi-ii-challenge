21b1c1d2c2219b860b152fd25a72d3346cdc95a9
Tyler Biswell <tsbiswell@gmail.com>
2019-10-01 23:58:19 -0600

    add endpoint specs for GET request

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,32 +4,55 @@ const Posts = require('../data/db');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    res.send('Hello from the POST /api/posts endpoint');
-  });
-  
-  router.post('/:id/comments', (req, res) => {
-    res.send('Hello from the POST /api/posts/:id/comments endpoint');
-  });
-  
-  router.get('/', (req, res) => {
-    res.send('Hello from the GET /api/posts endpoint');
-  });
-  
-  router.get('/:id', (req, res) => {
-    res.send('Hello from the GET /api/posts/:id endpoint');
-  });
-  
-  router.get('/:id/comments', (req, res) => {
-    res.send('Hello from the GET /api/posts/:id/comments endpoint');
-  });
-  
-  router.delete('/:id', (req, res) => {
-    res.send('Hello from the DELETE /api/posts/:id endpoint');
-  });
-  
-  router.put('/:id', (req, res) => {
-    res.send('Hello from the PUT /api/posts/:id endpoint');
-  });
-  
+  // res.send('Hello from the POST /api/posts endpoint');
+  Posts.insert();
+});
+
+// calling insert passing it a post object will add it to the database and return an object with the id of the inserted post. The object looks like this: { id: 123 }.
+
+router.post('/:id/comments', (req, res) => {
+  // res.send('Hello from the POST /api/posts/:id/comments endpoint');
+  Posts.insertComment();
+});
+
+// calling insertComment while passing it a comment object will add it to the database and return an object with the id of the inserted comment. The object looks like this: { id: 123 }. This method will throw an error if the post_id field in the comment object does not match a valid post id in the database.
+
+router.get('/', (req, res) => {
+  Posts.find(req.query)
+    .then(posts => res.status(200).json(posts))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' }),
+    );
+});
+
+router.get('/:id', (req, res) => {
+  // res.send('Hello from the GET /api/posts/:id endpoint');
+  Posts.findById();
+});
+
+// this method expects an id as it's only parameter and returns the post corresponding to the id provided or an empty array if no post with that id is found.
+
+router.get('/:id/comments', (req, res) => {
+  // res.send('Hello from the GET /api/posts/:id/comments endpoint');
+  Posts.findPostComments();
+});
+
+// the findPostComments accepts a postId as its first parameter and returns all comments on the post associated with the post id.
+
+router.delete('/:id', (req, res) => {
+  // res.send('Hello from the DELETE /api/posts/:id endpoint');
+  Posts.remove();
+});
+
+// the remove method accepts an id as its first parameter and upon successfully deleting the post from the database it returns the number of records deleted.
+
+router.put('/:id', (req, res) => {
+  // res.send('Hello from the PUT /api/posts/:id endpoint');
+  Posts.update();
+});
+
+// accepts two arguments, the first is the id of the post to update and the second is an object with the changes to apply. It returns the count of updated records. If the count is 1 it means the record was updated correctly.
 
 module.exports = router;
