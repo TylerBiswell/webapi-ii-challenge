ad05f6d53de61acd13493ead1cafb25b13f08ce5
Tyler Biswell <tsbiswell@gmail.com>
2019-10-02 00:02:43 -0600

    installed cors and added DELETE endpoint specs

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -10,7 +10,7 @@ router.post('/', (req, res) => {
     });
   } else {
     Posts.insert(req.body)
-      .then(user => res.status(201).json(req.body))
+      .then(post => res.status(201).json(req.body))
       .catch(err =>
         res.status(500).json({
           error: 'There was an error while saving the post to the database.',
@@ -20,8 +20,21 @@ router.post('/', (req, res) => {
 });
 
 router.post('/:id/comments', (req, res) => {
-  // res.send('Hello from the POST /api/posts/:id/comments endpoint');
-  Posts.insertComment();
+  const commentObj = { ...req.body, post_id: req.params.id };
+
+  if (!req.body.text) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please provide text for the comment.' });
+  } else {
+    Posts.insertComment(commentObj)
+      .then(comment => res.status(201).json(commentObj))
+      .catch(err =>
+        res.status(500).json({
+          error: 'There was an error while saving the comment to the database.',
+        }),
+      );
+  }
 });
 
 // calling insertComment while passing it a comment object will add it to the database and return an object with the id of the inserted comment. The object looks like this: { id: 123 }. This method will throw an error if the post_id field in the comment object does not match a valid post id in the database.
@@ -74,11 +87,20 @@ router.get('/:id/comments', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // res.send('Hello from the DELETE /api/posts/:id endpoint');
-  Posts.remove();
+  Posts.remove(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: 'The post could not be removed.' }),
+    );
 });
-
-// the remove method accepts an id as its first parameter and upon successfully deleting the post from the database it returns the number of records deleted.
 
 router.put('/:id', (req, res) => {
   // res.send('Hello from the PUT /api/posts/:id endpoint');
